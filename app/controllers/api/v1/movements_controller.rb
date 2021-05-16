68594c8a97d7ad0d9726ee62289feffce663c46d
Mohammed <atefhamosa@gmail.com>
2021-05-16 20:18:45 +0200

    Fix controller

--- app/controllers/api/v1/movements_controller.rb
+++ app/controllers/api/v1/movements_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::MovementssController < ApplicationController
+class Api::V1::MovementsController < ApplicationController
     def index
         @movements = Movement.all
         render :json => @movements, status: :ok
