de9cecdf6895784d5ceb67ed0089bb80405ba375
Mohammed <atefhamosa@gmail.com>
2021-05-17 14:45:33 +0200

    Add new movement methods

--- app/controllers/api/v1/movements_controller.rb
+++ app/controllers/api/v1/movements_controller.rb
@@ -3,4 +3,12 @@ class Api::V1::MovementsController < ApplicationController
         @movements = Movement.all
         render :json => @movements, status: :ok
     end
+
+    def new_tracked_movement
+        @user = User.where(name: params[:name])
+        movement = @user[0].tracked_movements.build(movement_id: params[:movement_id])
+        if movement.save 
+            render :json => {message: 'Successfully added!'}, status: :ok
+        end
+    end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            render :json => @user, status: :ok
+            render :json => {message: 'Successfully signed up!'}, status: :ok
         end
     end
 
     def session
-        @user = User.where(name: params[:name], password: params[:password])
+        @user = User.where(name: params[:name], password: params[:password], birth_date: params[:birth_date])
         render :json => @user, include: :movements, status: :ok
     end
 
