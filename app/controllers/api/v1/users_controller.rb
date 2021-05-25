b1a673f767b8d7d21856b260a0bf5d4405f941ff
Mohammed <atefhamosa@gmail.com>
2021-05-25 19:04:00 +0200

    Update user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -29,7 +29,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name, :password)
+    params.requier(:user).permit(:name, :password)
   end
 
   def set_user
