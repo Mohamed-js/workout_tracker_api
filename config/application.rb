601a849ebbd5ef7f8337fddee6718842c62892c3
Mohammed <atefhamosa@gmail.com>
2021-05-25 22:02:16 +0200

    Hide database configs

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,11 @@ module WorkoutTrackerApi
     config.load_defaults 6.1
     config.api_only = true
     config.autoload_paths << Rails.root.join('lib')
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
